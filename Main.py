96ead002e86ad95b6fd8396cec7f491b59fe6c93
yuan zhang <117572222+yuanzhang7@users.noreply.github.com>
2024-12-27 15:29:26 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ import math
 # internal imports
 from utils.file_utils import save_pkl, load_pkl
 from utils.utils import *
-from utils.core_utils import train
+from core_utils import train
 from utils.dataset_generic import Generic_WSI_Classification_Dataset, Generic_MIL_Dataset
 
 # pytorch imports
